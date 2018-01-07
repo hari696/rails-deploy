3fd32aa08f3e2b298bfa60c68ba0169a50993c16
Napster <harinapster007@gmail.com>
2018-01-07 23:51:41 +0530

    Namespace fixes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,33 +15,33 @@ SSHKit.config.command_map[:rake] = "bundle exec rake"
 SSHKit.config.command_map[:rails] = "bundle exec rails"
 
 
-namespace :deploy do
+# namespace :deploy do
 
-  desc 'Before bundle'
-  task :before_bundle do
-    on roles(:app), in: :sequence, wait: 5 do
-      within release_path do
-        with rails_env: fetch(:rails_env) do
-          execute "cd #{release_path} && rm -rf public/uploads"
-          execute "ln -s #{shared_path}/uploads #{release_path}/public/"
-        end
-      end
-    end
-  end
+#   desc 'Before bundle'
+#   task :before_bundle do
+#     on roles(:app), in: :sequence, wait: 5 do
+#       within release_path do
+#         with rails_env: fetch(:rails_env) do
+#           execute "cd #{release_path} && rm -rf public/uploads"
+#           execute "ln -s #{shared_path}/uploads #{release_path}/public/"
+#         end
+#       end
+#     end
+#   end
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute "chmod 777 -R #{shared_path}/tmp"
-      execute "chmod 777 -R #{shared_path}/uploads"
-      execute "sudo service nginx restart"
-      # execute "sudo touch #{File.join(current_path,'tmp','restart.txt')}"
-    end
-  end
+#   desc 'Restart application'
+#   task :restart do
+#     on roles(:app), in: :sequence, wait: 5 do
+#       execute "chmod 777 -R #{shared_path}/tmp"
+#       execute "chmod 777 -R #{shared_path}/uploads"
+#       execute "sudo service nginx restart"
+#       # execute "sudo touch #{File.join(current_path,'tmp','restart.txt')}"
+#     end
+#   end
 
-  before "bundler:install", 'deploy:before_bundle'
-  after :finishing, 'deploy:restart'
-  after :finishing, 'deploy:cleanup'
+#   before "bundler:install", 'deploy:before_bundle'
+#   after :finishing, 'deploy:restart'
+#   after :finishing, 'deploy:cleanup'
 
-  # after :bundle, 'deploy:after_bundle'
-end 
+#   # after :bundle, 'deploy:after_bundle'
+# end 
