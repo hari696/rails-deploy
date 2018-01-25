51dc60d77ae4ba439c1f2a650aef6f4953154047
Napster <harinapster007@gmail.com>
2018-01-25 18:22:16 +0530

    ProductionFile fixes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -11,8 +11,3 @@ SSHKit.config.command_map[:rake] = "bundle exec rake"
 
 # Force rails through bundle exec
 SSHKit.config.command_map[:rails] = "bundle exec rails"
-
-config.cache_classes = true
-config.serve_static_assets = true
-config.assets.compile = true
-config.assets.digest = true
