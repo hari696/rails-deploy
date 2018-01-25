0bdabf8416503da3aa8d4dfb06b63b230456bbf8
Napster <harinapster007@gmail.com>
2018-01-25 22:42:36 +0530

    Database and Production updation

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@ set :stage, :production
 # Setting RAILS_ENV environment variable on server
 set :rails_env, :production
  
-role :app, %w{root@139.59.93.164}
+role :app, %w{root@139.59.37.218}
 
 # Force rake through bundle exec
 SSHKit.config.command_map[:rake] = "bundle exec rake"
